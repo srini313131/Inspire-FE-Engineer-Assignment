cce5c4fe85ca62d64c2484d79b75930acc1e7243
sri ghantasala <srini.ghantasala31@gmail.com>
2023-08-04 21:53:35 +0530

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksComponent } from './pages/books/books.component';
+import { PurchaseComponent } from './pages/purchase/purchase.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { AuthorCardComponent } from './components/author-card/author-card.component';
+import { BookComponent } from './components/book/book.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AddEditBookComponent } from './components/add-edit-book/add-edit-book.component';
+
+const routes: Routes = [
+  { path: 'books', component: BooksComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/books' },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    PurchaseComponent,
+    PageNotFoundComponent,
+    AuthorCardComponent,
+    BookComponent,
+
+    AddEditBookComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
